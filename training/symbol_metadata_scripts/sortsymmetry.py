8ea1a88b9d139a870dfa92583a91c6c2e5ad8a47
Voxel <41875513+VoxelCubes@users.noreply.github.com>
2024-12-10 19:45:39 +0100

    sort symmetries

---
+++ training/symbol_metadata_scripts/sortsymmetry.py
@@ -0,0 +1,21 @@
+# input_file = "../../handtex/data/symbol_metadata/symmetry_other.txt"
+input_file = "../../handtex/data/symbol_metadata/symmetry_self.txt"
+
+# Split each line into a list of symbols.
+# Sort this list of symbols.
+# Then sort the entire file by rows.
+
+with open(input_file, "r") as file:
+    lines = file.readlines()
+    lines = [line.strip() for line in lines]
+
+# Sort the entire file by rows.
+lines.sort(key=lambda x: x.lower())
+lines.sort(key=lambda x: x.startswith("latex2e"), reverse=True)
+
+
+# Write the sorted file back out.
+output_file = "../../handtex/data/symbol_metadata/symmetry_sorted.txt"
+
+with open(output_file, "w") as file:
+    file.write("\n".join(lines))

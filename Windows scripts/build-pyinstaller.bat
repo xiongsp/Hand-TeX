ca47294b62984058d12092b175f17b0f33d2422a
Voxel <41875513+VoxelCubes@users.noreply.github.com>
2025-03-02 22:58:11 +0000

    remove space in windows exe name

--- Windows scripts/build-pyinstaller.bat
+++ Windows scripts/build-pyinstaller.bat
@@ -4,13 +4,13 @@
 .\venv\Scripts\pip install pyinstaller
 .\venv\Scripts\pyinstaller.exe handtex/main.py --paths 'venv/Lib/site-packages' ^
     --onedir --noconfirm --clean --workpath=build --distpath=dist_exe --windowed ^
-    --name="Hand TeX" --icon=handtex/data/custom_icons/logo.ico ^
+    --name="HandTeX" --icon=handtex/data/custom_icons/logo.ico ^
     --copy-metadata numpy ^
     --copy-metadata packaging ^
     --copy-metadata pyyaml ^
     --collect-datas handtex
 
 @REM Copy "docs\What is _internal.txt" "dist_exe/PanelCleaner\What is _internal.txt"
-xcopy "handtex\data" "dist_exe\Hand TeX\_internal\handtex\data" /E /I /Y
-cd "dist_exe\Hand TeX\_internal\handtex\data"
+xcopy "handtex\data" "dist_exe\HandTeX\_internal\handtex\data" /E /I /Y
+cd "dist_exe\HandTeX\_internal\handtex\data"
 for /d /r . %%d in (__pycache__) do @rmdir /s /q "%%d"
